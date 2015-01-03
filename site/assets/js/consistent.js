bbc50b977e2892ce8181fa20771bbcdd5eac410a
Steve McKinney <steve@iamsteve.me>
2015-01-03 20:38:30 +0000

    sethrefs, typography changes

--- site/assets/js/consistent.js
+++ site/assets/js/consistent.js
@@ -1,3 +1,61 @@
+sethrefs = function () {
+	
+	if (document.querySelectorAll) {
+	
+		var datahrefs = document.querySelectorAll('[data-href]'),
+				dhcount = datahrefs.length;
+		
+		while (dhcount-- > 0) {
+			
+			var ele = datahrefs[dhcount],
+					addevent = function (ele, event, func) {
+					
+						if(ele.addEventListener) ele.addEventListener(event, link, false);
+						else ele.attachEvent('on' + event, link);
+					
+					},
+					link = function (event) {
+						
+						var target = event.target,
+								url = this.getAttribute('data-href');
+						
+						if (!target.href) {
+					    		
+			    		var newevent = document.createEvent("MouseEvents");
+			    		
+			    		if (newevent.initMouseEvent) {
+			    		
+			    			var newlink = document.createElement("a");
+		    			  
+		    			  newlink.setAttribute('href', url);
+		    			  newlink.innerHTML = 'link event';
+		    			  
+	    					newevent.initMouseEvent(event.type, true, false, window, event.detail, event.screenX, event.screenY, event.clientX, event.clientY, event.ctrlKey, event.altKey, event.shiftKey, event.metaKey, event.button, null);
+	    					
+	    					newlink.dispatchEvent(newevent);
+				    	
+				    	}
+				    	else {
+				    	
+				    		var meta = (event.metaKey) ? '_self' : '_blank';
+				    		
+				    		window.open(url, meta);
+				    	
+				    	}
+					    
+						}
+						
+					};
+			
+			addevent(ele, 'click', link);
+		
+		}
+	
+	}
+	
+};
+
+sethrefs();
 /**
  * Prism: Lightweight, robust, elegant syntax highlighting
  * MIT license http://www.opensource.org/licenses/mit-license.php/
