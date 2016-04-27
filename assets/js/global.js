5bdaed9ce35730b856da09d2ca940122d569fab3
Steve McKinney <steve@iamsteve.me>
2016-04-27 14:08:34 +0100

    add all code into module and add toggling for search and navigation and checks for whether one or the other is active

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,9 +1,44 @@
-var toggle = document.getElementById('toggle');
-var nav = document.getElementById('nav');
+var iamsteve = (function ()
+{  
+  var toggle = document.getElementById('toggle');
+  var nav = document.getElementById('nav');
+  var toggle_search = document.getElementById('toggle-search');
+  var search = document.getElementById('search');
 
-toggle.addEventListener('click', function(e) {
-  e.preventDefault();
-  
-  this.classList.toggle('active');
-  nav.classList.toggle('active');
-});
+  var toggler = function ()
+  {    
+    toggle.addEventListener('click', function(e)
+    {
+      e.preventDefault();
+      
+      this.classList.toggle('active');
+      nav.classList.toggle('visible');
+      
+      if (toggle_search.classList.contains('active')) 
+      {
+        toggle_search.classList.remove('active');
+        search.classList.remove('visible');
+      }
+    });
+    
+    toggle_search.addEventListener('click', function(e)
+    { 
+      e.preventDefault();
+      
+      this.classList.toggle('active');
+      search.classList.toggle('visible');
+      document.getElementById('keywords').focus();
+      
+      if (toggle.classList.contains('active')) 
+      {
+        toggle.classList.remove('active');
+        nav.classList.remove('visible');
+      }
+    });
+  };
+
+  return {
+    toggler: toggler()
+  };
+
+})();
