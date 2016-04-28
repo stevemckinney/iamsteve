0e8e3e823cd753517f87a2f1956669f0712f993c
Steve McKinney <steve@iamsteve.me>
2016-04-28 07:41:40 +0100

    change paths

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'breakpoint'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/css"
+css_dir = "dist/css"
 sass_dir = "assets/sass"
 images_dir = "assets/images"
 javascripts_dir = "assets/js"
