6b8f3d51b8b91650888b3a353f777eaa2b17f4f4
Steve McKinney <steve@iamsteve.me>
2016-06-25 18:47:17 +0100

    add is mobile checker

--- system/user/addons/iamsteve/pi.iamsteve.php
+++ system/user/addons/iamsteve/pi.iamsteve.php
@@ -129,35 +129,6 @@ class Iamsteve {
     return $html;
   }
 
-  
-  public function image_design()
-  {
-    $images = array(
-      $this->base_url . '/assets/images/design-default-1.svg',
-      $this->base_url . '/assets/images/design-default-2.svg',
-      $this->base_url . '/assets/images/design-default-3.svg',
-      $this->base_url . '/assets/images/design-default-4.svg',
-    );
-    
-    $image = array_rand($images, 1);
-
-    return '<img src="' . $images[$image] . '">';
-  }
-  
-  public function image_code()
-  {
-    $images = array(
-      $this->base_url . '/assets/images/code-default-1.svg',
-      $this->base_url . '/assets/images/code-default-2.svg',
-      $this->base_url . '/assets/images/code-default-3.svg',
-      $this->base_url . '/assets/images/code-default-4.svg',
-    );
-    
-    $image = array_rand($images, 1);
-    
-    return '<img src="' . $images[$image] . '">';
-  }
-  
   public function background($image = '')
   {
     $image =  ee()->TMPL->fetch_param('image');
@@ -197,6 +168,31 @@ class Iamsteve {
 
     // return random number
     return strval(rand(intval($from), intval($to)));
+  }
+
+  public function is_mobile()
+  {    
+    if ( empty($_SERVER['HTTP_USER_AGENT']) )
+    {
+      $is_mobile = false;  
+    } 
+    elseif (
+         strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false // many mobile devices (all iPhone, iPad, etc.)
+      || strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false
+      || strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false
+      || strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false
+      || strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false
+      || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false
+      || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false )
+    {
+      $is_mobile = true;
+    }
+    else
+    {
+      $is_mobile = false;
+    }
+    
+    return $is_mobile;
   }
 }
 // END CLASS
