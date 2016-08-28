544bd5b44abb4afaee9964f4b473964c05c331ac
Steve McKinney <steve@iamsteve.me>
2016-08-28 16:28:17 +0100

    add cookie addon

---
+++ system/user/addons/cookie/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'author'         => 'EllisLab',
+	'author_url'     => 'https://ellislab.com/',
+	'name'           => 'Cookie',
+	'description'    => 'Lets you easily get and set cookies in your ExpressionEngine templates.',
+	'version'        => '1.0.0',
+	'namespace'      => 'User\Addons\Cookie',
+	'settings_exist' => FALSE
+);
