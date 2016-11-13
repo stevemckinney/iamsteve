f35b1711889d885ee19bc0d280b5dc40c6ab3a6b
Steve McKinney <steve@iamsteve.me>
2016-11-13 21:14:15 +0000

    aync load osc

--- assets/js/load-svg.js
+++ assets/js/load-svg.js
@@ -1,17 +1,29 @@
 var svg = (function ()
 { 
-  var call = function() {
-    var ajax = new XMLHttpRequest();
-    ajax.open('GET', 'dist/images/rio.svg', true);
-    ajax.send();
-    ajax.onload = function(e)
+  var rio = function() {
+    var rio = new XMLHttpRequest();
+    rio.open('GET', 'dist/images/rio.svg', true);
+    rio.send();
+    rio.onload = function(e)
     {
       var img = document.getElementById('rio');
-      img.innerHTML = ajax.responseText;
-    }  
+      img.innerHTML = rio.responseText;
+    }
+  }
+  
+  var osc = function() {
+    var osc = new XMLHttpRequest();
+    osc.open('GET', 'dist/images/osc.svg', true);
+    osc.send();
+    osc.onload = function(e)
+    {
+      var img = document.getElementById('osc');
+      img.innerHTML = osc.responseText;
+    }
   }
   
   return {
-    call: call()
+    rio: rio(),
+    osc: osc()
   }
 })();
