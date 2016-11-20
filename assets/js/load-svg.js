2fcabd9a8192764941a0be58a9bee27879a1a3a4
Steve McKinney <steve@iamsteve.me>
2016-11-20 18:16:47 +0000

    check if the id exists before loading the svg

--- assets/js/load-svg.js
+++ assets/js/load-svg.js
@@ -1,24 +1,32 @@
 var svg = (function ()
 { 
   var rio = function() {
-    var rio = new XMLHttpRequest();
-    rio.open('GET', document.location.origin + '/dist/images/rio.svg', true);
-    rio.send();
-    rio.onload = function(e)
+    var img = document.getElementById('rio');
+    
+    if ( img )
     {
-      var img = document.getElementById('rio');
-      img.innerHTML = rio.responseText;
+      var rio = new XMLHttpRequest();
+      rio.open('GET', document.location.origin + '/dist/images/rio.svg', true);
+      rio.send();
+      rio.onload = function(e)
+      {
+        img.innerHTML = rio.responseText;
+      }
     }
   }
   
   var osc = function() {
-    var osc = new XMLHttpRequest();
-    osc.open('GET', document.location.origin + '/dist/images/osc.svg', true);
-    osc.send();
-    osc.onload = function(e)
+    var img = document.getElementById('osc');
+    
+    if ( img )
     {
-      var img = document.getElementById('osc');
-      img.innerHTML = osc.responseText;
+      var osc = new XMLHttpRequest();
+      osc.open('GET', document.location.origin + '/dist/images/osc.svg', true);
+      osc.send();
+      osc.onload = function(e)
+      {
+        img.innerHTML = osc.responseText;
+      }
     }
   }
   
