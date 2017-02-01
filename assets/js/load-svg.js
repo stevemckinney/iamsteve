908ac0b57a31cda4b9881a973f106b2960974ef5
Steve McKinney <steve@iamsteve.me>
2017-02-01 12:48:44 +0000

    as the images have updated the ajax to load them did too

--- assets/js/load-svg.js
+++ assets/js/load-svg.js
@@ -1,37 +1,33 @@
 var svg = (function ()
 { 
-  var rio = function() {
-    var img = document.getElementById('rio');
+  var dogs = function() {
+    var img = document.getElementById('rio-osc');
     
     if ( img )
     {
-      var rio = new XMLHttpRequest();
-      rio.open('GET', document.location.origin + '/dist/images/rio.svg', true);
-      rio.send();
-      rio.onload = function(e)
+      var dogs = new XMLHttpRequest();
+      dogs.open('GET', document.location.origin + '/dist/images/rio-osc.svg', true);
+      dogs.send();
+      dogs.onload = function(e)
       {
-        img.innerHTML = rio.responseText;
+        img.innerHTML = dogs.responseText;
       }
     }
   }
   
-  var osc = function() {
-    var img = document.getElementById('osc');
-    
-    if ( img )
+  var subscribe = function()
+  {
+    var sub = new XMLHttpRequest();
+    sub.open('GET', document.location.origin + '/dist/images/subscribe.svg', true);
+    sub.send();
+      
+    sub.onload = function(e)
     {
-      var osc = new XMLHttpRequest();
-      osc.open('GET', document.location.origin + '/dist/images/osc.svg', true);
-      osc.send();
-      osc.onload = function(e)
-      {
-        img.innerHTML = osc.responseText;
-      }
+      document.body.insertAdjacentHTML('afterbegin', sub.responseText);
     }
   }
-  
   return {
-    rio: rio(),
-    osc: osc()
+    dogs: dogs(),
+    sub: subscribe()
   }
 })();
