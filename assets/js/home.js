a12a74817cd65dd6d59b0434125bfff66d82992b
Steve McKinney <steve@iamsteve.me>
2017-08-23 18:55:59 +0100

    finally understand the module pattern

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,22 +3,22 @@ import './modernizr';
 const Flickity = require('flickity');
 
 /* global Modernizr */
-const home = (function () {
+const home = (function home() {
   // With multiple, and having custom pagination, you need to initialise using
   // the parent as a reference, https://github.com/metafizzy/flickity/issues/319
   const setupFlickity = (container) => {
     // init flickity
     const scroll = container.querySelector('.scroll');
-  
+
     // Pagination
     const left = container.querySelector('.paginate-left');
     const right = container.querySelector('.paginate-right');
-  
+
     // Has flickity reached the first or last item?
     // If so add or remove the relevant class name
     // reference: https://github.com/metafizzy/flickity/issues/220
     const disabled = 'disabled';
-  
+
     const flickity = new Flickity(scroll,
       {
         cellAlign: 'left',
@@ -31,32 +31,32 @@ const home = (function () {
         groupCells: true,
         percentPosition: false
       });
-  
+
     // Initial state means left should be disabled
     left.classList.add(disabled);
-  
+
     left.addEventListener('click', () => flickity.previous());
-  
+
     right.addEventListener('click', () => flickity.next());
-  
+
     flickity.on('cellSelect', () => {
       const i = flickity.selectedIndex;
-  
+
       if (i === flickity.cells.length - 1) right.classList.add(disabled);
       else right.classList.remove(disabled);
-  
+
       if (i === 0) left.classList.add(disabled);
       else left.classList.remove(disabled);
     });
-  };
-  
-  
+  }
+
+
   // Find all rows of posts, loop through them and setup flickity
   return {
-    var flickities = () => {
+    flickities: () => {
       const rows = document.querySelectorAll('.posts');
       const pagination = document.querySelectorAll('.js-pagination');
-    
+
       if (!Modernizr.touchevents) {
         for (let i = 0; i < rows.length; i += 1) {
           setupFlickity(rows[i]);
@@ -67,8 +67,8 @@ const home = (function () {
           pagination[ii].parentNode.removeChild(pagination[ii]);
         }
       }
-    };
+    }
   }
-})();
+}());
 
 home.flickities();
