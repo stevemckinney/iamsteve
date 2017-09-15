dfc500e5bbb75af8d0e2efab7a296b4b55aee9cc
Steve McKinney <steve@iamsteve.me>
2017-09-15 07:31:10 +0100

    update cache version

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@
 // You have to supply a name for your cache, this will
 // allow us to remove an old one to avoid hitting disk
 // space limits and displaying old resources
-var cacheName = 'v7';
+var cacheName = 'v8';
 
 // Assets to cache
 var assetsToCache = [
