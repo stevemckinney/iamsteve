b3c324bd4a5d6ebbb06cc08f032ad7f530ff6523
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-02-20 16:39:54 +0000

    update cache

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@
 // You have to supply a name for your cache, this will
 // allow us to remove an old one to avoid hitting disk
 // space limits and displaying old resources
-var cacheName = 'v3';
+var cacheName = 'v4';
 
 // Assets to cache
 var assetsToCache = [
