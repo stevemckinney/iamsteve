b45a9d0d56ddcb079040d6d5ebbf76c034187850
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-10-18 07:37:45 +0100

    no new tabs for links of any kind

--- components/Link.js
+++ components/Link.js
@@ -17,7 +17,7 @@ const CustomLink = ({ href, ...rest }) => {
     return <a href={href} {...rest} />
   }
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+  return <a rel="noopener noreferrer" href={href} {...rest} />
 }
 
 export default CustomLink
