29e0edf54633b8849ad62e014d26a85aa2fa173a
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-11-01 19:57:26 +0000

    match live site footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,29 +4,17 @@ import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
-        </div>
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
-        </div>
-      </div>
+    <footer className="row between neutral-l2 contain contain-medium contain-large footer">
+      <p className="m0 copyright">
+        {`©`} <Link href="/">{siteMetadata.title}</Link> {`${new Date().getFullYear()}`}
+      </p>
+      <nav>
+        <a href="https://www.iubenda.com/privacy-policy/12610320" className="iubenda-white iubenda-embed" title="Privacy Policy">Privacy Policy</a>
+        <Link href="/feed" title="Subscribe to the blog RSS feed">RSS</Link>
+        <Link href="/about">About</Link>
+        <Link href="/uses" title="What the site was built with & software I use">Uses</Link>
+        <Link href="#top" className="top">Top</Link>
+      </nav>
     </footer>
   )
 }
