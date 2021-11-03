5807ed9b28128d5f60f3e6f214448982379a888c
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-11-03 12:55:44 +0000

    rename to title

--- components/Title.js
+++ components/Title.js
@@ -1,7 +1,7 @@
 import Link from 'next'
 import Icon from './icon'
 
-const SectionHeader = ({ title, href }) => (
+const Title = ({ title, href }) => (
 <header class="row row-header contain contain-medium contain-large between">
   <h2 class="f4-l chunky neutral row-title" id="latest-posts-title">{title}</h2>
   <Link href={href} class="link-icon warm secondary-hover card-permalink semibold sans f5-l">
