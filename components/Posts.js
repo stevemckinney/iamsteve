ed2cc271de205e8ec4832806185b9d44788c3105
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-11-03 12:58:06 +0000

    Update Posts.js

--- components/Posts.js
+++ components/Posts.js
@@ -1,11 +1,11 @@
 import Link from 'next'
-import SectionHeader from 'SectionHeader';
+import Title from 'Title';
 import Card from 'Card';
 import Icon from './icon'
 
-const SectionHeader = ({ title, href }) => (
+const Posts = ({ posts }) => (
   <section class="posts pt7 pb6 pb8-d" aria-labelledby="latest-posts-title">
-    <SectionHeader title='Latest posts' href="/blog" />
+    <Title title='Latest posts' href='/blog' />
     <div class="scroll scroll-medium contain-scroll" id="latest-posts">
       <Card />
       <Link href="{path='blog/P12'}" class="link-icon warm secondary-hover card-permalink semibold sans center f2-l">
