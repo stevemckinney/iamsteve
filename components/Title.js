67afc8b75c67d2db298460510bbc9d0164aa951a
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-11-09 08:13:41 +0000

    conditional link

--- components/Title.js
+++ components/Title.js
@@ -1,3 +1,7 @@
+/**
+ * title
+ * title, link (optional)
+ */
 import Link from 'next/link'
 import Icon from './icon'
 
@@ -6,10 +10,11 @@ const Title = ({ title, link }) => {
     <header className="row row-header contain contain-medium contain-large between">
       <h2 className="f4-l chunky neutral row-title" id="latest-posts-title">{title}</h2>
       
-      <Link href={link}>
-        <a className="link-icon warm secondary-hover card-permalink semibold sans f5-l">All posts <Icon kind="right" /></a>
-      </Link>
-      
+      {link && (
+        <Link href={link}>
+          <a className="link-icon warm secondary-hover card-permalink semibold sans f5-l">All posts <Icon kind="right" /></a>
+        </Link>
+      )}
     </header>
   )
 }
