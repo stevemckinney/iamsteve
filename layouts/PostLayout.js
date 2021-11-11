1216f2aac3f9acc5a86530c5cf12ae677cbf20f7
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-11-11 12:35:21 +0000

    named import?

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -8,7 +8,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
-import PageViews from '@/components/PageViews';
+import { PageViews } from '@/components/PageViews';
 
 const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
 
