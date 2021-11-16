c8aac885fee7652f6f591080f5c973ccdc651355
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-11-16 07:29:03 +0000

    maybe this works best for output

--- components/PageViews.js
+++ components/PageViews.js
@@ -2,14 +2,14 @@ import useSWR from 'swr';
 
 export const fetcher = async (RequestInfo) => {
   const Response = await fetch(RequestInfo)
-  
+
   return await Response.json()
 }
 
-export const views = ({ slug }) => {
+export const views = (slug) => {
   const { data } = useSWR(`/api/views/${slug}`, fetcher)
-  
-  return data?.total ? `${data.total}` : `0`
+
+  return <>{data?.total ? `${data.total} views` : `0 views`}</>
 }
 
 export const PageViews = ({ slug }) => {
