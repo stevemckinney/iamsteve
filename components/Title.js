8666e4647292c7c09bc676823f1dea76c7d4875b
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-11-16 07:29:45 +0000

    allow for themed post sections

--- components/Title.js
+++ components/Title.js
@@ -5,14 +5,17 @@
 import Link from 'next/link'
 import Icon from './icon'
 
-const Title = ({ title, link }) => {
+const Title = ({ title, link, text = 'All posts' }) => {
   return (
     <header className="row row-header contain contain-medium contain-large between">
       <h2 className="f4-l chunky neutral row-title" id="latest-posts-title">{title}</h2>
-      
+
       {link && (
         <Link href={link}>
-          <a className="link-icon warm secondary-hover card-permalink semibold sans f5-l">All posts <Icon kind="right" /></a>
+          <a className="link-icon warm secondary-hover card-permalink semibold sans f5-l">
+            {text}
+            <span class="icon icon-medium icon-right secondary"><Icon kind="right" /></span>
+          </a>
         </Link>
       )}
     </header>
--- layouts/Posts.js
+++ layouts/Posts.js
@@ -6,15 +6,15 @@ import Link from '@/components/Link'
 import Pagination from '@/components/Pagination'
 import Title from '@/components/Title'
 
-export default function Posts({ title, link, children }) {
+export default function Posts({ title, link, children, size }) {
   // aria label
   const ariaID = `${title.toLowerCase().split(' ').join('-')}-title`
-  
+
   return (
     <>
       <section className="posts pt7 pb6 pb8-d" aria-labelledby={ariaID}>
         <Title title={title} link={link} id={ariaID} />
-        <div className="scroll scroll-medium contain-scroll">
+        <div className={`scroll scroll-${size} contain-scroll`}>
           {children}
         </div>
       </section>
