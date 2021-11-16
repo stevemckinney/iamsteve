b63c749a7764a4a7ba656f6534f1ba4d2a400fe4
stevemckinney <341708+stevemckinney@users.noreply.github.com>
2021-11-16 07:28:48 +0000

    correct layout

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -43,20 +43,20 @@ const LayoutWrapper = ({ children }) => {
           ))}
         </nav>
       </header>
-      
+
       <Tabbar />
-      
+
       <main id="content">
         {children}
       </main>
-      
+
       <aside className="row row-normal pt6 pt8-d contain contain-medium contain-large between items-end">
         <section className="column column-4-b column-3-d mb6 mb0-b">
           <a href="/" className="logo-footer mb4 primary"><Logo className="logo-small fill-currentcolor" /></a>
           <p className="mb4">iamsteve is a blog written by Steve McKinney, focusing on the design and build of websites. The aim is to bridge the gap in building your design. It started&thinsp;—&thinsp;and remains&thinsp;—&thinsp;a way to encourage self learning and sharing, through a mixture of in depth tutorials and quick tips.</p>
           <p><strong className="sans">You can find me elsewhere</strong> <br /><Social /></p>
         </section>
-        <section className="column column-4-b column-5-d rio-osc text-right" id="rio-osc">
+        <section className="column column-4-b column-5-d rio-osc text-right items-end-b" id="rio-osc">
           <Image src="/static/images/rio-osc.svg" width={512} height={288} />
         </section>
       </aside>
