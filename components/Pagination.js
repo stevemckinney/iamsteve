465e696c1f333a4ee28d3ef2a2aaec225be7a855
Steve <341708+stevemckinney@users.noreply.github.com>
2021-11-18 20:02:37 +0000

    make the conditions work correctly to break up pagination as i need

--- components/Pagination.js
+++ components/Pagination.js
@@ -4,65 +4,63 @@ export default function Pagination({ style = 'full', totalPages, currentPage }) 
   const prevPage = parseInt(currentPage) - 1 > 0
   const nextPage = parseInt(currentPage) + 1 <= parseInt(totalPages)
 
-  {(() => {
-    if (style === 'full') {
-        return (
-          <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-            <nav className="flex justify-between">
-              {!prevPage && (
+  return (
+    <>
+      {style === 'full' ? (
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <nav className="flex justify-between">
+            {prevPage ? (
+              <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
+                <button rel="previous">Previous</button>
+              </Link>
+            ) : (
+              <button rel="previous" className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
+                Previous
+              </button>
+            )}
+            <span>
+              {currentPage} of {totalPages}
+            </span>
+            {nextPage ? (
+              <Link href={`/blog/page/${currentPage + 1}`}>
+                <button rel="next">Next</button>
+              </Link>
+            ) : (
+              <button rel="next" className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
+                Next
+              </button>
+            )}
+          </nav>
+        </div>
+      ) : (
+        <>
+          {style === 'previous' ? (
+            <>
+              {prevPage ? (
+                <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
+                  <button rel="previous">Previous</button>
+                </Link>
+              ) : (
                 <button rel="previous" className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
                   Previous
                 </button>
               )}
-              {prevPage && (
-                <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
-                  <button rel="previous">Previous</button>
+            </>
+          ) : (
+            <>
+              {nextPage ? (
+                <Link href={`/blog/page/${currentPage + 1}`}>
+                  <button rel="next">Next</button>
                 </Link>
-              )}
-              <span>
-                {currentPage} of {totalPages}
-              </span>
-              {!nextPage && (
+              ) : (
                 <button rel="next" className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
                   Next
                 </button>
               )}
-              {nextPage && (
-                <Link href={`/blog/page/${currentPage + 1}`}>
-                  <button rel="next">Next</button>
-                </Link>
-              )}
-            </nav>
-          </div>
-        )
-    }
-    else if (style === 'previous') {
-      return (
-        {!prevPage && (
-          <button rel="previous" className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
-            Previous
-          </button>
-        )}
-        {prevPage && (
-          <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
-            <button rel="previous">Previous</button>
-          </Link>
-        )}
-      )
-    }
-    else if (style === 'previous') {
-      return (
-        {!nextPage && (
-          <button rel="next" className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
-            Next
-          </button>
-        )}
-        {nextPage && (
-          <Link href={`/blog/page/${currentPage + 1}`}>
-            <button rel="next">Next</button>
-          </Link>
-        )}
-      )
-    }
-  })()}
+            </>
+          )}
+        </>
+      )}
+    </>
+  )
 }
