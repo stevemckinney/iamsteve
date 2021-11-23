033c741348a3769b5dc12490c4b938bbfb97fd77
Steve <341708+stevemckinney@users.noreply.github.com>
2021-11-23 13:07:53 +0000

    make about page the about page

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,18 @@
-import { MDXLayoutRenderer } from '@/components/MDXComponents'
-import { getFileBySlug } from '@/lib/mdx'
+import Link from '@/components/Link'
 
-const DEFAULT_LAYOUT = 'AuthorLayout'
-
-export async function getStaticProps() {
-  const authorDetails = await getFileBySlug('authors', ['default'])
-  return { props: { authorDetails } }
-}
-
-export default function About({ authorDetails }) {
-  const { mdxSource, frontmatter } = authorDetails
-
+export default function AboutPage() {
   return (
-    <MDXLayoutRenderer
-      layout={frontmatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontmatter={frontmatter}
-    />
+    <div className="flex flex-col items-start justify-start md:justify-center md:items-center md:flex-row md:space-x-6 md:mt-24">
+      <div className="pt-6 pb-8 space-x-2 md:space-y-5">
+        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:text-8xl md:leading-14 md:border-r-2 md:px-6">
+          About
+        </h1>
+      </div>
+      <div className="max-w-md">
+        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+          Work in progress.
+        </p>
+      </div>
+    </div>
   )
 }
