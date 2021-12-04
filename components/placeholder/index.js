52b4214f66ed5b89a3c0c94574efab553caf45eb
Steve <341708+stevemckinney@users.noreply.github.com>
2021-12-04 15:47:08 +0000

    different method for retrieving random placeholder
    this doesn't return the same random value for some reason even though i've assigned it to an object and all i want to do is take the colour and url from the same object entry

--- components/placeholder/index.js
+++ components/placeholder/index.js
@@ -3,54 +3,57 @@
 import Image from 'next/image'
 
 // placeholders
-const PlaceholderDesignMap = new Map()
-PlaceholderDesignMap.set('#e9f5f5', '/static/images/default/design-default-1.svg')
-PlaceholderDesignMap.set('#fff8e2', '/static/images/default/design-default-2.svg')
-PlaceholderDesignMap.set('#ffe5e2', '/static/images/default/design-default-3.svg')
-PlaceholderDesignMap.set('#d5f5ee', '/static/images/default/design-default-4.svg')
+const PlaceholderDesign = {
+  '#e9f5f5': '/static/images/default/design-default-1.svg',
+  '#fff8e2': '/static/images/default/design-default-2.svg',
+  '#ffe5e2': '/static/images/default/design-default-3.svg',
+  '#d5f5ee': '/static/images/default/design-default-4.svg',
+}
 
-const PlaceholderCodeMap = new Map()
-PlaceholderCodeMap.set('#e9f5f5', '/static/images/default/code-default-1.svg')
-PlaceholderCodeMap.set('#fff8e2', '/static/images/default/code-default-2.svg')
-PlaceholderCodeMap.set('#ffe5e2', '/static/images/default/code-default-3.svg')
-PlaceholderCodeMap.set('#d5f5ee', '/static/images/default/code-default-4.svg')
+const PlaceholderCode = {
+  '#e9f5f5': '/static/images/default/code-default-1.svg',
+  '#fff8e2': '/static/images/default/code-default-2.svg',
+  '#ffe5e2': '/static/images/default/code-default-3.svg',
+  '#d5f5ee': '/static/images/default/code-default-4.svg',
+}
 
-function getRandomKey(collection) {
-  let keys = Array.from(collection.keys())
-  return keys[Math.floor(Math.random() * keys.length)]
+const randomProperty = function (obj) {
+  var keys = Object.keys(obj)
+  return obj[keys[(keys.length * Math.random()) << 0]]
+}
+
+const randomValues = function (obj) {
+  var vals = Object.values(obj)
+  return obj[vals[(vals.length * Math.random()) << 0]]
+}
+
+const randomEntries = function (obj) {
+  var entries = Object.entries(obj)
+  return entries[(entries.length * Math.random()) << 0]
 }
 
 function Placeholder({ category, kind }) {
   // assign the random value to a variable to use later
   // depending on the kind passed to this function
   const random =
-    category === 'Design' ? getRandomKey(PlaceholderDesignMap) : getRandomKey(PlaceholderCodeMap)
-
+    category === 'Design' ? randomEntries(PlaceholderDesign) : randomEntries(PlaceholderCode)
+  const color = random[0]
+  const image = random[1]
   const containerClass =
     kind.toString() === 'hero'
       ? 'pt4 pb4 pt6-b pb6-b pt7-d pb8-d flex center featured-image entry-image'
       : 'radius flex'
 
   return (
-    <div className={containerClass} style={{ backgroundColor: random }}>
-      {category === 'Design' ? (
-        <Image
-          src={PlaceholderDesignMap.get(random)}
-          className="radius"
-          width={378}
-          height={252}
-          alt=""
-        />
-      ) : (
-        <Image
-          src={PlaceholderCodeMap.get(random)}
-          className="radius"
-          width={378}
-          height={252}
-          alt=""
-        />
-      )}
-    </div>
+    <>
+      <div className={containerClass} style={{ backgroundColor: color }}>
+        {category === 'Design' ? (
+          <Image src={image} className="radius" width={378} height={252} alt="" />
+        ) : (
+          <Image src={image} className="radius" width={378} height={252} alt="" />
+        )}
+      </div>
+    </>
   )
 }
 
