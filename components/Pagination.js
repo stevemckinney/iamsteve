de1aa40af6c477a410b11226d5fa1692531c0e8e
Steve <341708+stevemckinney@users.noreply.github.com>
2021-12-08 20:35:52 +0000

    duplicated <a>
    swore this was a solution to multiple nested elements

--- components/Pagination.js
+++ components/Pagination.js
@@ -10,13 +10,11 @@ export default function Pagination({ stage = 'full', totalPages, currentPage }) 
       {stage === 'full' ? (
         <nav className="row center pagination pagination-offset">
           {newer ? (
-            <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
-              <a rel="next" className="paginate paginate-left link-icon">
-                <span className="icon icon-medium icon-left">
-                  <Icon kind="left" />
-                </span>
-                <strong>Next</strong>
-              </a>
+            <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`} rel="next" className="paginate paginate-left link-icon">
+              <span className="icon icon-medium icon-left">
+                <Icon kind="left" />
+              </span>
+              <strong>Next</strong>
             </Link>
           ) : (
             <span className="paginate paginate-left disabled link-icon" disabled={!newer}>
@@ -32,13 +30,11 @@ export default function Pagination({ stage = 'full', totalPages, currentPage }) 
             </span>
           </div>
           {older ? (
-            <Link href={`/blog/page/${currentPage + 1}`}>
-              <a rel="previous" className="paginate paginate-right link-icon">
-                <strong>Older</strong>
-                <span className="icon icon-medium icon-right">
-                  <Icon kind="right" />
-                </span>
-              </a>
+            <Link href={`/blog/page/${currentPage + 1}`} rel="previous" className="paginate paginate-right link-icon">
+              <strong>Older</strong>
+              <span className="icon icon-medium icon-right">
+                <Icon kind="right" />
+              </span>
             </Link>
           ) : (
             <span className="paginate paginate-right disabled link-icon" disabled={!older}>
@@ -54,13 +50,11 @@ export default function Pagination({ stage = 'full', totalPages, currentPage }) 
           {stage === 'previous' || stage === 'older' ? (
             <>
               {older ? (
-                <Link href={`/blog/page/${currentPage + 1}`}>
-                  <a rel="previous" className="warm sans link-icon">
-                    <strong>Older</strong>
-                    <span className="icon icon-medium icon-right">
-                      <Icon kind="right" />
-                    </span>
-                  </a>
+                <Link href={`/blog/page/${currentPage + 1}`} rel="previous" className="warm sans link-icon">
+                  <strong>Older</strong>
+                  <span className="icon icon-medium icon-right">
+                    <Icon kind="right" />
+                  </span>
                 </Link>
               ) : (
                 <span className="primary-l2 sans link-icon disabled" disabled={!older}>
@@ -74,13 +68,11 @@ export default function Pagination({ stage = 'full', totalPages, currentPage }) 
           ) : (
             <>
               {newer ? (
-                <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
-                  <a rel="next" className="warm sans link-icon">
-                    <span className="icon icon-medium icon-left">
-                      <Icon kind="left" />
-                    </span>
-                    <strong>Newer</strong>
-                  </a>
+                <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`} rel="next" className="warm sans link-icon">
+                  <span className="icon icon-medium icon-left">
+                    <Icon kind="left" />
+                  </span>
+                  <strong>Newer</strong>
                 </Link>
               ) : (
                 <span className="primary-l2 sans link-icon disabled" disabled={!newer}>
