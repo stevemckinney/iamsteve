ae9403bd980bfac82ac2b9902681733c74c9d5d5
Steve <341708+stevemckinney@users.noreply.github.com>
2021-12-10 12:21:48 +0000

    should fix internal server errors

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   compress: false,
+  target: 'serverless',
   sassOptions: {
     includePaths: [
       path.join(__dirname, './node_modules/breakpoint-sass/stylesheets/'),
