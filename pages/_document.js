c9d90db3cd04d3c14e7bb450b0dd3892901a0692
Steve <341708+stevemckinney@users.noreply.github.com>
2021-12-15 12:59:20 +0000

    jsx...

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="viewport" content="width=device-width, viewport-fit=cover, initial-scale=1, shrink-to-fit=no">
+          <meta name="viewport" content="width=device-width, viewport-fit=cover, initial-scale=1, shrink-to-fit=no" />
           <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
-          <link rel="shortcut icon" href="/static/favicons/favicon.ico">
+          <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#543243" />
