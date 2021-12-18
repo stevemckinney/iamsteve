71cc2980662021dea193264386a30af9331c2350
Steve <341708+stevemckinney@users.noreply.github.com>
2021-12-18 10:15:24 +0000

    make search part of the tabbar

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog', icon: 'blog' },
   { href: '/about', title: 'About', icon: 'about' },
   { href: '/contact', title: 'Contact', icon: 'contact' },
+  { href: '/search', title: 'Search', icon: 'searchSmall' },
 ]
 
 export default headerNavLinks
