17597972ea79196f5647316c45c9569aa0837c12
Steve <341708+stevemckinney@users.noreply.github.com>
2021-12-22 11:02:10 +0000

    correct sizing

--- components/Title.js
+++ components/Title.js
@@ -8,13 +8,13 @@ import Icon from './icon'
 const Title = ({ title, link, text }) => {
   return (
     <header className="row row-header contain contain-medium contain-large between">
-      <h2 className="f4-l chunky neutral row-title" id="latest-posts-title">
+      <h2 className="f6 f5-a f4-c chunky neutral row-title" id="latest-posts-title">
         {title}
       </h2>
 
       {link && (
         <Link href={link}>
-          <a className="link-icon warm secondary-hover card-permalink semibold sans f5-l">
+          <a className="link-icon warm secondary-hover card-permalink semibold sans f7 f6-a f5-c">
             {text ? text : 'All posts'}
             <span className="icon icon-medium icon-right secondary">
               <Icon kind="right" />
