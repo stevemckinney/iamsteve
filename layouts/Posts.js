161049689326e3239b22fa1c022250fead1d208b
Steve <341708+stevemckinney@users.noreply.github.com>
2023-07-14 14:33:50 +0100

    fix spacing

--- layouts/Posts.js
+++ layouts/Posts.js
@@ -1,12 +1,13 @@
 import Title from '@/components/Title'
 
-export default function Posts({ title, link, text, children, size }) {
+export default function Posts({ title, link, text, children, size, position }) {
   // aria label
   const ariaID = `${title.toLowerCase().split(' ').join('-')}-title`
+  const postsClass = position === `top` ? `posts pt7 pb6 pt8-d` : `posts pt7 pb6 pb8-d`
 
   return (
     <>
-      <section className="posts pt7 pb6 pb8-d" aria-labelledby={ariaID}>
+      <section className={postsClass} aria-labelledby={ariaID}>
         <Title title={title} link={link} text={text} id={ariaID} />
         <div className={`scroll scroll-${size} contain-scroll`}>{children}</div>
       </section>
