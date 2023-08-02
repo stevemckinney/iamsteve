f1b2a020315ad77894524e9b7ad61e0d1d40ff43
Steve <341708+stevemckinney@users.noreply.github.com>
2023-08-02 16:55:17 +0100

    testing for build

--- components/mdx-components.js
+++ components/mdx-components.js
@@ -2,7 +2,7 @@
 import { useMemo } from 'react'
 import { getMDXComponent } from 'mdx-bundler/client'
 import Image from '@/components/image'
-import Pre from '@/components/Pre'
+// import Pre from '@/components/Pre'
 import CustomLink from '@/components/Link'
 // import TOCInline from '@/components/TOCInline'
 // import { BlogNewsletterForm } from './NewsletterForm'
@@ -11,7 +11,7 @@ import { useMDXComponent } from 'next-contentlayer/hooks'
 const components = {
   Image,
   a: CustomLink,
-  pre: Pre,
+  // pre: Pre,
   // BlogNewsletterForm: BlogNewsletterForm,
 }
 
--- contentlayer.config.js
+++ contentlayer.config.js
@@ -3,12 +3,12 @@ import smartypants from 'remark-smartypants'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
 import rehypeToc from '@jsdevtools/rehype-toc'
-import rehypePrism from 'rehype-prism-plus'
-import remarkRehype from 'remark-rehype';
-import remarkCodeTitles from 'remark-flexible-code-titles';
+// import rehypePrism from 'rehype-prism-plus'
+// import remarkRehype from 'remark-rehype';
+// import remarkCodeTitles from 'remark-flexible-code-titles';
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
-import { visit } from 'unist-util-visit'
+// import { visit } from 'unist-util-visit'
 
 export const Page = defineDocumentType(() => ({
   name: "Page",
@@ -120,24 +120,6 @@ export default makeSource({
     remarkPlugins: [
       remarkGfm,
       smartypants,
-      [
-        remarkCodeTitles,
-        {
-          titleProperties(language, title) {
-            return {
-              ["data-language"]: language,
-              ["data-title"]: `${title}`,
-            }
-          },
-          containerProperties(language, title) {
-            return {
-              ["data-language"]: language,
-              ["data-title"]: `${title}`,
-            }
-          },
-        },
-      ],
-      remarkRehype,
     ],
     rehypePlugins: [
       rehypeSlug,
@@ -151,7 +133,6 @@ export default makeSource({
         },
       ],
       [rehypeToc, { customizeTOC }],
-      rehypePrism,
     ],
   },
 })
