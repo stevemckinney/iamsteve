b18eb38e3ec5d39d7fdb449562f17ba9df45eba3
Steve <341708+stevemckinney@users.noreply.github.com>
2023-08-02 17:09:05 +0100

    trim down initial build

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -5,8 +5,10 @@ import rehypeSlug from 'rehype-slug'
 import rehypeToc from '@jsdevtools/rehype-toc'
 import rehypePrism from 'rehype-prism-plus'
 import remarkRehype from 'remark-rehype';
-// import remarkCodeTitles from 'remark-flexible-code-titles';
+import remarkCodeTitles from 'remark-flexible-code-titles';
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+import { visit } from 'unist-util-visit'
 
 export const Page = defineDocumentType(() => ({
   name: "Page",
@@ -118,23 +120,9 @@ export default makeSource({
     remarkPlugins: [
       remarkGfm,
       smartypants,
-      // [
-      //   remarkCodeTitles,
-      //   {
-      //     titleProperties(language, title) {
-      //       return {
-      //         ["data-language"]: language,
-      //         ["data-title"]: `${title}`,
-      //       }
-      //     },
-      //     containerProperties(language, title) {
-      //       return {
-      //         ["data-language"]: language,
-      //         ["data-title"]: `${title}`,
-      //       }
-      //     },
-      //   },
-      // ],
+      [
+        remarkCodeTitles,
+      ],
       remarkRehype,
     ],
     rehypePlugins: [
@@ -150,7 +138,6 @@ export default makeSource({
       ],
       [rehypeToc, { customizeTOC }],
       rehypePrism,
-      },
     ],
   },
 })
