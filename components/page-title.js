f507f34c26bcc1911208b0f4331f5fa1558a90d4
Steve <341708+stevemckinney@users.noreply.github.com>
2023-08-14 20:55:58 +0100

    page title styles

--- components/page-title.js
+++ components/page-title.js
@@ -1,3 +1,10 @@
 export default function PageTitle({ children }) {
-  return <h1 className="font-display lowercase text-7xl text-fern-1100" style={{fontVariationSettings: "'wdth' 100, 'wght' 750"}}>{children}</h1>
+  return (
+    <h1
+      className="font-display lowercase text-7xl text-fern-1100"
+      style={{ fontVariationSettings: "'wdth' 100, 'wght' 750" }}
+    >
+      {children}
+    </h1>
+  )
 }
