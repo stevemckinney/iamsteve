7694913bed7e047b3453a55c4e18d1d2a47a056e
Steve <341708+stevemckinney@users.noreply.github.com>
2023-09-12 20:22:52 +0100

    id for top

--- components/header.js
+++ components/header.js
@@ -11,7 +11,7 @@ export default function Header() {
   const navLink = `flex items-center gap-2 text-xl font-ui lowercase leading-none relative ${styles.link}`
   return (
     <>
-      <header className="grid grid-cols-subgrid col-start-margin-start col-end-margin-end relative z-10">
+      <header className="grid grid-cols-subgrid col-start-margin-start col-end-margin-end relative z-10" id="top">
         <div className="col-content flex items-center justify-between py-9 px-8 -mx-8 bg-neutral-01-100">
           <Link href="/" className="flex-1">
             <Icon icon="logo" size={32} />
