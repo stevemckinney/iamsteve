94d940da3d45bef1f7c8d1a8b911c006ea5a3954
Steve <341708+stevemckinney@users.noreply.github.com>
2023-11-13 20:06:46 +0000

    lots of responsive stuff and refactoring

--- components/header.js
+++ components/header.js
@@ -9,13 +9,13 @@ import Icon from '@/components/icon'
 import styles from './header.module.scss'
 
 export default function Header() {
-  const nav = `flex flex-1 md:gap-12`
-  const maxmdNav = `max-md:px-4 max-md:bg-white/90 max-md:shadow-placed max-md:justify-between max-md:fixed max-md:left-0 max-md:right-0 max-md:bottom-0 max-md:z-10 max-md:backdrop-blur max-md:backdrop-brightness-100 max-md:backdrop-saturate-150`
+  const nav = `flex flex-1 lg:gap-12`
+  const maxmdNav = `max-lg:px-4 max-lg:bg-white/90 max-lg:shadow-placed max-lg:justify-between max-lg:fixed max-lg:left-0 max-lg:right-0 max-lg:bottom-0 max-lg:z-10 max-lg:backdrop-blur max-lg:backdrop-brightness-100 max-lg:backdrop-saturate-150`
 
   // Some styles exist in the header.module to handle safe-area
   const navLink = `flex items-center gap-1 text-base font-ui lowercase leading-none relative ${styles.link}`
-  const mdNavLink = `md:gap-2 md:text-xl`
-  const maxmdNavLink = `max-md:flex-col max-md:flex-1 max-md:justify-center max-md:pt-2`
+  const mdNavLink = `lg:gap-2 lg:text-xl`
+  const maxmdNavLink = `max-lg:flex-col max-lg:flex-1 max-lg:justify-center max-lg:pt-2`
 
   return (
     <>
@@ -23,15 +23,15 @@ export default function Header() {
         className="grid grid-cols-subgrid col-start-margin-start col-end-margin-end relative z-10"
         id="top"
       >
-        <div className="col-content flex items-center align-center justify-between py-4 md:py-9 md:px-8 md:-mx-8 bg-neutral-01-100">
+        <div className="col-content flex items-center align-center justify-between py-4 lg:py-9 lg:px-8 lg:-mx-8 bg-neutral-01-100">
           <Link href="/" className="flex-1" title="iamsteve.me homepage">
-            <Icon icon="logo" role="img" size={32} className="max-md:hidden" />
+            <Icon icon="logo" role="img" size={32} className="max-lg:hidden" />
             <Icon
               icon="logo"
               role="img"
               aria-hidden="true"
               size={24}
-              className="md:hidden"
+              className="lg:hidden"
             />
           </Link>
           <nav className={`${nav} ${maxmdNav}`}>
--- components/subscribe.js
+++ components/subscribe.js
@@ -8,16 +8,16 @@ const Subscribe = ({
 }) => {
   return (
     <section className={`${className}`}>
-      <div className="flex flex-col gap-4 w-[40.1315789474%]">
+      <div className="flex flex-col gap-2 lg:gap-4 w-full max-lg:col-end-content-end col-start-content-start col-span-6 xl:col-span-5">
         <Image
           src="/images/illustration/spot/envelope.svg"
           width={96}
           height={96}
-          className="drop-shadow-placed"
+          className="max-lg:w-[48px] max-lg:h-[48px] drop-shadow-placed"
           alt="Illustration of a sealed envelope"
           role="presentation"
         />
-        <h2 className="text-5xl font-display font-variation-bold lowercase fern-1100">
+        <h2 className="text-2xl lg:text-5xl font-display font-variation-bold lowercase fern-1100">
           {title}
         </h2>
         <p className="m-0 text-base text-ui-body">
@@ -26,7 +26,7 @@ const Subscribe = ({
           unsubscribe anytime.
         </p>
       </div>
-      <div className="w-[40.1315789474%]">
+      <div className="col-content lg:col-start-7 xl:col-start-8 lg:col-end-content-end">
         <NewsletterForm unique={unique} />
       </div>
     </section>
