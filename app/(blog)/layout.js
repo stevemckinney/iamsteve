dac8e6a6c0a2d9c984314a8a4aea6c0643bcd728
Steve <341708+stevemckinney@users.noreply.github.com>
2024-01-21 11:09:40 +0000

    updated frames

--- app/(blog)/layout.js
+++ app/(blog)/layout.js
@@ -1,6 +1,6 @@
 export default function BlogLayout({ children }) {
   return (
-    <main className="grid grid-cols-subgrid col-start-container-start col-end-container-end gap-y-18 sm:frame sm:frame-16 lg:frame-40 sm:frame-outset-top-sm lg:frame-outset-top pt-10 pb-18 mb-18">
+    <main className="grid grid-cols-subgrid col-start-container-start col-end-container-end gap-y-18 sm:frame sm:frame-24 lg:frame-40 lg:frame-outset-top-md 2xl:frame-outset-top pt-10 pb-18 mb-18">
       {children}
     </main>
   )
