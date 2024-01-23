09143149cc0b380971f2682d909713dc9e8ef7b2
Steve <341708+stevemckinney@users.noreply.github.com>
2024-01-23 20:12:20 +0000

    realised I don't actually need svgr

--- next.config.js
+++ next.config.js
@@ -18,29 +18,4 @@ const nextConfig = {
   },
 }
 
-// https://github.com/vercel/next.js/issues/48177
-function withSplitSVGr(config) {
-  return Object.assign({}, config, {
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        type: 'asset',
-        resourceQuery: /url/, // *.svg?url
-      })
-
-      config.module.rules.push({
-        test: /\.svg$/i,
-        use: ['@svgr/webpack'],
-        resourceQuery: /svgr/, // *.svg?svgr
-      })
-
-      if (typeof config.webpack === 'function') {
-        return config.webpack(config, options)
-      }
-      if (options.isServer) config.devtool = 'source-map'
-      return config
-    },
-  })
-}
-
-module.exports = withSplitSVGr(withContentlayer(nextConfig))
+module.exports = withContentlayer(nextConfig)
