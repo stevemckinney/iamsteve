30239c96f6bdde56a69291c1069fee428b23a70d
Steve <341708+stevemckinney@users.noreply.github.com>
2024-01-23 20:01:43 +0000

    update csp and remove it from next.js svg should load just fine?

--- next.config.js
+++ next.config.js
@@ -15,9 +15,6 @@ const nextConfig = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 }
 
