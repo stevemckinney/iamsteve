aa1d978de58da5f03668629d6bd84ed16b64ab8e
Steve <341708+stevemckinney@users.noreply.github.com>
2024-03-15 20:09:27 +0000

    Update frame.js

--- components/frame.js
+++ components/frame.js
@@ -6,7 +6,7 @@
 const Frame = ({ children, id }) => {
   return (
     <div
-      className="grid col-margin sm:col-container grid-cols-subgrid sm:frame sm:frame-16 lg:frame-40 gap-y-4 md:gap-y-8 pb-10 md:pb-18 sm:mt-[1.25rem] md:mt-[1.375rem]"
+      className="grid col-container sm:col-container grid-cols-subgrid sm:frame sm:frame-16 lg:frame-40 gap-y-4 md:gap-y-8 pb-10 md:pb-18 sm:mt-[1.25rem] md:mt-[1.375rem]"
       id={id}
     >
       {children}
