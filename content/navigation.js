7327b8b468ecb34d408981cac140a9aded91e576
Steve <341708+stevemckinney@users.noreply.github.com>
2024-03-20 20:54:51 +0000

    add empty files

--- content/navigation.js
+++ content/navigation.js
@@ -3,7 +3,7 @@ const navigation = [
   { href: '/category/design', title: 'Design', icon: 'pen', size: 24 },
   { href: '/category/code', title: 'Code', icon: 'code', size: 24 },
   { href: '/blog', title: 'Archive', icon: 'folder', size: 24 },
-  { href: '/about', title: 'About', icon: 'person', size: 24 },
+  { href: '/collections', title: 'Collections', icon: 'collections', size: 24 },
   { href: '/contact', title: 'Contact', icon: 'envelope', size: 24 },
 ]
 
