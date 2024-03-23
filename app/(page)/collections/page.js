4c6b44ff87214b3de6edc18db2411e325e6b1fa7
Steve <341708+stevemckinney@users.noreply.github.com>
2024-03-23 11:20:04 +0000

    various updates

--- app/(page)/collections/page.js
+++ app/(page)/collections/page.js
@@ -12,6 +12,15 @@ import { format, subWeeks, isAfter } from 'date-fns'
 import { allCollections } from 'contentlayer/generated'
 import collections from '@/content/collections'
 
+export async function generateMetadata() {
+  return {
+    template: '%s • iamsteve',
+    title: 'Collections',
+    description:
+      'Curated links to all things design. If you’re looking for inspiration start here.',
+  }
+}
+
 const getData = cache(async () => {
   const groupedCollections = allCollections.reduce((acc, item) => {
     item.collection.forEach((collection) => {
@@ -35,10 +44,10 @@ async function renderCollections() {
     <>
       {Object.entries(groupedCollections).map(([collection, items]) => (
         <div className="flex flex-col gap-4" key={collection}>
-          <h2 className="text-xl leading-xl font-display font-variation-bold lowercase">
+          <h2 className="lg:text-xl leading-xl font-display font-variation-bold lowercase">
             {collection}
           </h2>
-          <ul className="flex flex-col">
+          <ul className="grid grid-rows-[repeat(auto-fit, 48px)]">
             {items.map((item) => {
               const [y, m, d] = item.date.split('-').map((n) => parseInt(n, 10))
               const itemDate = new Date(y, m, d)
@@ -47,21 +56,23 @@ async function renderCollections() {
               return (
                 <li
                   key={item.url}
-                  className="flex flex-1 justify-between py-4 border-t border-neutral-01-500/20 last:border-b"
+                  className="flex border-t border-neutral-01-500/20 last:border-b"
                 >
                   <a
                     href={item.url}
-                    className="flex flex-1 gap-2 text-3xl truncate group hover:text-dandelion-600 transition duration-200 linear"
-                    target="_blank"
+                    className="text-xl lg:text-3xl py-2 flex whitespace-nowrap flex-1 gap-2 group hover:text-dandelion-600 transition duration-200 linear self-center"
                     rel="noopener noreferrer"
                   >
                     {item.title}
-                    <span className="text-fern-1100/20 group-hover:text-fern-1100/40 transition duration-200 linear">
-                      {item.url.replace('https://', '').replace('www.', '')}
+                    <span className="text-fern-1100/20 group-hover:text-fern-1100/40 transition duration-200 linear truncate">
+                      {item.url
+                        .replace('https://', '')
+                        .replace('www.', '')
+                        .replace(/\/$/, '')}
                     </span>
                   </a>
                   {isNew && (
-                    <span className="flex self-center px-2 py-.5 font-ui lowercase bg-fern-300/10 ring-1 ring-fern-500/40 text-fern-600 justify-center rounded-sm">
+                    <span className="flex self-center px-2 py-1 font-ui lowercase bg-fern-300/10 leading-none ring-1 ring-fern-500/40 text-fern-600 justify-center rounded-sm">
                       New
                     </span>
                   )}
@@ -86,11 +97,12 @@ export default async function CollectionsPage({ params }) {
         alt=" "
         aria-hidden="true"
       />
-      <Header className="row-start-1 max-sm:frame max-sm:frame-24 flex flex-col gap-4 md:gap-8 col-start-content-start col-end-6">
+      <Header className="sm:row-start-1 max-sm:frame max-sm:frame-24 flex flex-col gap-4 md:gap-8 col-start-content-start max-sm:col-end-content-end col-end-6">
         <Column className="md:col-span-1">
           <Title className="text-4xl">Collections</Title>
           <Description>
-            Curated links to all things design, frequently updated.
+            Curated links to all things design. If you’re looking for
+            inspiration start here.
           </Description>
         </Column>
         <Column className="md:col-span-1">
@@ -101,7 +113,7 @@ export default async function CollectionsPage({ params }) {
           </ul>
         </Column>
       </Header>
-      <section className="flex flex-col col-start-6 col-end-content-end row-start-1 pb-18 gap-y-10 lg:gap-y-18">
+      <section className="flex flex-col col-start-content-start sm:col-start-6 col-end-content-end sm:row-start-1 lg:pb-18 gap-y-10 lg:gap-y-18">
         {renderCollections(params)}
       </section>
     </>
