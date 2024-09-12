b6b6810ccba74d179d4140e1c613751ffba03620
Steve <341708+stevemckinney@users.noreply.github.com>
2024-09-12 08:06:13 +0100

    use netlify basic example as test

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,30 +1,15 @@
 const ContactForm = () => {
   return (
-    <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-      <input type="hidden" name="form-name" value="contact" />
-
-      <p hidden>
-        <label>
-          Don't fill this out if you're human: <input name="bot-field" />
-        </label>
+    <form name="contact" netlify>
+      <p>
+        <label>Name <input type="text" name="name" /></label>
       </p>
-
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
-      </div>
-
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-      </div>
-
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-      </div>
-
-      <button type="submit">Send</button>
+      <p>
+        <label>Email <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   )
 }
