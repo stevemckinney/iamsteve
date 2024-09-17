c9466cebab25dab5d1d31f22706b840ad26b0d63
Steve <341708+stevemckinney@users.noreply.github.com>
2024-09-17 20:51:46 +0100

    can remove this safely

--- app/(blog)/blog/[...slug]/layout.js
+++ app/(blog)/blog/[...slug]/layout.js
@@ -1,16 +1,5 @@
-import { allPosts } from 'contentlayer/generated'
-
-export async function generateStaticParams() {
-  return allPosts.map((post) => ({
-    slug: post.slugAsParams.split('/'),
-  }))
-}
-
 export default function BlogPostLayout({
   children,
-  post,
-  sidebar,
-  additional,
 }) {
   return (
     <>
