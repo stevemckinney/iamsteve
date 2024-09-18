76f38103cc04b39d79917a171b4bea3c4b4f3f17
Steve <341708+stevemckinney@users.noreply.github.com>
2024-09-18 19:20:38 +0100

    add csp

--- next.config.js
+++ next.config.js
@@ -3,6 +3,50 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+// Content Security Policy
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://scripts.simpleanalyticscdn.com https://static.codepen.io https://use.typekit.net;
+  style-src 'self' 'unsafe-inline' https://use.typekit.net;
+  img-src * blob: data:;
+  media-src 'self';
+  connect-src *;
+  font-src 'self' https://use.typekit.net;
+  frame-src https://codepen.io;
+  worker-src 'self' blob:;
+`
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
@@ -57,12 +101,26 @@ const nextConfig = {
         ],
       },
       {
+        source: '/feed.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, s-maxage=2592000, stale-while-revalidate=604800',
+          },
+        ],
+      },
+      {
         source: '/:path*',
         headers: [
           {
             key: 'Cache-Control',
             value: 'public, max-age=0, s-maxage=3600, stale-while-revalidate=60',
           },
+          ...securityHeaders,
         ],
       }
     ]
