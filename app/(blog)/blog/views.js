0ab270b5a69e76ce42ee64992887f30659fa5352
Steve <341708+stevemckinney@users.noreply.github.com>
2025-01-10 19:30:07 +0000

    correct supabase update

--- app/(blog)/blog/views.js
+++ app/(blog)/blog/views.js
@@ -13,7 +13,16 @@ export const getAllPageViews = cache(async () => {
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
-      cookies: () => cookies(),
+      cookies: {
+        getAll() {
+          return cookies().getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookies().set(name, value, { ...options, path: '/' })
+          })
+        },
+      },
     }
   )
 
--- app/page.js
+++ app/page.js
@@ -19,7 +19,16 @@ const getData = cache(async () => {
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
-      cookies: () => cookies(),
+      cookies: {
+        getAll() {
+          return cookies().getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookies().set(name, value, { ...options, path: '/' })
+          })
+        },
+      },
     }
   )
 
