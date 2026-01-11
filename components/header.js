e83421f8b6a4ea66c5743969e0e604b75ec2f48e
Steve <341708+stevemckinney@users.noreply.github.com>
2026-01-11 18:28:38 +0000

    ensuring some styles work well

--- components/header.js
+++ components/header.js
@@ -20,7 +20,7 @@ export default function Header() {
 
   const nav =
     'flex flex-[1_0_auto] lg:gap-8 2xl:gap-12 xl:justify-center max-lg:px-2'
-  const tabbarNav = `max-lg:transition-all max-lg:duration-200 max-lg:ease-out max-lg:px-8 max-lg:bg-white/80 max-lg:bg-fern-1200/90 max-lg:shadow-placed max-lg:justify-between max-lg:fixed max-lg:left-1/2 max-lg:-translate-x-1/2 max-lg:z-100 max-lg:backdrop-blur-sm max-lg:backdrop-brightness-100 max-lg:backdrop-contrast-100 max-lg:backdrop-saturate-150 max-lg:gap-6 max-lg:rounded-full ${styles.tabbar}`
+  const tabbarNav = `max-lg:transition-all max-lg:duration-200 max-lg:ease-out max-lg:px-8 max-lg:bg-white/80 dark:max-lg:bg-fern-1200/90 max-lg:shadow-placed max-lg:justify-between max-lg:fixed max-lg:left-1/2 max-lg:-translate-x-1/2 max-lg:z-100 max-lg:backdrop-blur-sm max-lg:backdrop-brightness-100 max-lg:backdrop-contrast-100 max-lg:backdrop-saturate-150 max-lg:gap-6 max-lg:rounded-full ${styles.tabbar}`
 
   // Some styles exist in the header.module to handle safe-area
   const navLink = `flex items-center gap-1 text-base font-ui lowercase leading-none relative ${styles.link}`
@@ -116,7 +116,7 @@ export default function Header() {
                       'lg:hidden': link.title === 'Home',
                       'max-lg:hidden': link.title === 'Contact',
                       'max-lg:text-emphasis': isActive,
-                      'max-lg:text-fern-400': !isActive,
+                      'max-lg:text-fern-700 dark:max-lg:text-fern-400': !isActive,
                     }
                   )}
                   key={link.href}
