2731da1b6a1cfaf8c2177ecb89e3e4517fcd3987
Steve <341708+stevemckinney@users.noreply.github.com>
2026-05-11 09:14:50 +0100

    Remove redundant query-param disallows from robots

--- app/robots.js
+++ app/robots.js
@@ -1,40 +1,30 @@
 import siteMetadata from '@/content/metadata'
 
 export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: [
-          'GPTBot',
-          'OAI-SearchBot',
-          'ClaudeBot',
-          'PerplexityBot',
-          'Google-Extended',
-          'Applebot-Extended',
-          'CCBot',
-        ],
-        allow: ['/'],
-      },
-      {
-        userAgent: '*',
-        allow: ['/'],
-        disallow: [
-          '/blog/drafts',
-          '/_next/image',
-          '/_next/data',
-          '/*?URL=',
-          '/*?_rsc=',
-          '/*?PageSpeed=',
-          '/*?utm_source=',
-          '/*?utm_medium=',
-          '/*?utm_campaign=',
-          '/*?utm_term=',
-          '/*?utm_content=',
-          '/*?fbclid=',
-          '/*?ref=',
-        ],
-      },
-    ],
-    sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
-  }
+    return {
+          rules: [
+            {
+                      userAgent: [
+                                  'GPTBot',
+                                  'OAI-SearchBot',
+                                  'ClaudeBot',
+                                  'PerplexityBot',
+                                  'Google-Extended',
+                                  'Applebot-Extended',
+                                  'CCBot',
+                                ],
+                      allow: ['/'],
+            },
+            {
+                      userAgent: '*',
+                      allow: ['/'],
+                      disallow: [
+                                  '/blog/drafts',
+                                  '/_next/image',
+                                  '/_next/data',
+                                ],
+            },
+                ],
+          sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
+    }
 }
